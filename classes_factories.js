c2f199621767193ada4df75cf8d3a3a145e4df5e
Serjay Parks <75953992+iLOVEthecolorORANGE@users.noreply.github.com>
2022-10-12 19:16:36 -0400

    created person class

--- classes_factories.js
+++ classes_factories.js
@@ -18,3 +18,39 @@ class Hamster{
         return this.Price
      }
 }
+
+class Person{
+    constructor(nameParam){
+        this.Name = nameParam
+        this.Age = 0
+        this.Height = 0
+        this.Weight = 0
+        this.Mood = 0
+        this.Hamsters = []
+        this.BankAccount = 0
+    }
+    getName(){
+        return this.Name
+    }
+    getAge(){
+        return this.Age
+    }
+    getWeight(){
+        return this.Weight
+    }
+    greet(){
+        console.log(`Hello, I am ${this.Name}!`)
+    }
+    ageUp(){
+        this.Age++
+        this.Height++
+        this.Weight++
+        this.Mood--
+        this.BankAccount+=10
+    }
+    buyHamster(hamster){
+        this.Hamsters.push(hamster)
+        this.Mood+=10
+        this.BankAccount-=getPrice()
+    }
+}
